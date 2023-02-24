Fix weight calculation for loss

model_explanation.py
@@ -39,7 +39,7 @@ def main():
     print("Num docs for topic model:", len(x))
     topics, word2idx = get_topics(config, x)
 
-    train(config, model1, data_train, data_val, batch_size=16)
+    train(config, model1, data_train, data_val, batch_size=32, lr=5e-5)
 
     # Only evaluate models on the test data
     x = [data_test[i]['sentence'] for i in range(min(10, len(data_test)))]
@@ -52,7 +52,7 @@ def main():
     del model1
     torch.cuda.empty_cache()
 
-    train(config, model2, data_train, data_val, batch_size=8)
+    train(config, model2, data_train, data_val, batch_size=8, lr=5e-6)
     shap_vals = load(f"shap_vals_gpt2_{config['dataset']}")
     shap_vals, topic_vals, word_vals = get_topic_shap(model2, x, topics, word2idx, shap_vals)
     save(topic_vals, f"topic_vals_gpt2_{config['dataset']}_{config['topics']}")

src/train.py
@@ -3,8 +3,9 @@ import numpy as np
 import evaluate
 import os
 import torch
-from sklearn.metrics import f1_score, mean_squared_error, accuracy_score, balanced_accuracy_score
+from sklearn.metrics import f1_score, mean_squared_error, accuracy_score, balanced_accuracy_score, classification_report
 from torch.nn import BCEWithLogitsLoss
+import glob
 
 
 def train(config, pipeline, train_data, val_data, batch_size=8, lr=1e-5):
@@ -13,7 +14,7 @@ def train(config, pipeline, train_data, val_data, batch_size=8, lr=1e-5):
     print(model_name)
     log_dir = f"checkpoints/{dataset_name}_{model_name}"
     resume = False
-    if os.path.exists(log_dir + "/checkpoint-500"):
+    if glob.glob(log_dir + "/checkpoint-*"): #os.path.exists(log_dir + "/checkpoint-500"):
         resume = True
 
     if os.path.exists(log_dir + "/pytorch_model.bin"):
@@ -22,12 +23,12 @@ def train(config, pipeline, train_data, val_data, batch_size=8, lr=1e-5):
 
     training_args = TrainingArguments(
         output_dir=log_dir,
-        evaluation_strategy="epoch",
         num_train_epochs=3,
         per_device_train_batch_size=batch_size,
-        per_device_eval_batch_size=16,
+        per_device_eval_batch_size=32,
         learning_rate=lr,
-        weight_decay=5e-8,
+        evaluation_strategy="steps",
+        fp16=True,
         save_total_limit=1)
     if dataset_name == "emobank" or dataset_name == "polite":
         metric = evaluate.load("mse")
@@ -39,24 +40,36 @@ def train(config, pipeline, train_data, val_data, batch_size=8, lr=1e-5):
 
     train_data_tokenized = train_data.map(tokenize_function, batched=True)
     val_data_tokenized = val_data.map(tokenize_function, batched=True)
-    print(train_data_tokenized[0])
+    # print(train_data_tokenized[0])
 
     def compute_metrics(eval_pred):
         logits, labels = eval_pred
         if dataset_name == "goemotions" or dataset_name == "blog":
-            pred = torch.from_numpy(logits).sigmoid() > 0.5
-            return {"f1-average": f1_score(labels, pred, average='weighted'), "micro-accuracy": accuracy_score(labels, pred), "macro-accuracy": balanced_accuracy_score(labels, pred)}
+            pred = torch.from_numpy(logits).float().sigmoid() > 0.5
+            print(classification_report(labels.astype(int), pred.int()))
+            return {"f1-average": f1_score(labels, pred, average='weighted'),
+                    "micro-accuracy": accuracy_score(labels, pred)}
         elif dataset_name != "emobank" and dataset_name != "polite":
             predictions = np.argmax(logits, axis=-1)
         else:
             predictions = logits
 
-        return metric.compute(predictions=predictions, references=labels)
+        # For the yelp dataset, we calculate full metrics and polarity metrics
+        if dataset_name == "yelp":
+            pred_binary = predictions > 1
+            labels_binary = labels > 1
+            pred = torch.from_numpy(logits).float().sigmoid() > 0.5
+            print(classification_report(labels.astype(int), pred.int()))
+            return {"f1": f1_score(labels, pred, average='weighted'),
+                    "accuracy": accuracy_score(labels, pred),
+                    "polarity-accuracy": accuracy_score(labels_binary, pred_binary)}
+        else:
+            return metric.compute(predictions=predictions, references=labels)
 
     if dataset_name == "goemotions" or dataset_name == "blog":
-        y = torch.stack([torch.from_numpy(row["labels"]) for row in train_data_tokenized])
-        print("All label shape:", y.shape)
-        weights = (y == 0.) / torch.sum(y, dim=0)
+        all_labels = torch.tensor([row["labels"] for row in train_data_tokenized])
+        weights = (all_labels == 0.).sum() / torch.sum(all_labels, dim=0)
+        print("Weights:", weights)
         class CustomTrainer(Trainer):
             def compute_loss(self, model, inputs, return_outputs=False):
                 labels = inputs.get("labels")
@@ -64,8 +77,9 @@ def train(config, pipeline, train_data, val_data, batch_size=8, lr=1e-5):
                 outputs = model(**inputs)
                 logits = outputs.get('logits')
                 # compute custom loss
-                loss_fct = BCEWithLogitsLoss(weight=weights)
-                loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+                loss_fct = BCEWithLogitsLoss(pos_weight=weights.cuda())
+                loss = loss_fct(logits.view(-1, self.model.config.num_labels),
+                                labels.view(-1, self.model.config.num_labels))
                 return (loss, outputs) if return_outputs else loss
 
         trainer = CustomTrainer(